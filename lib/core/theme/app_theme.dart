8408c140396b1214e4bd85c59549af54ace4017e
strawberryskeleton <xradhi5685@gmail.com>
2026-06-16 23:18:04 +0530

    added app theme + palette

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'app_palette.dart';
+
+class AppTheme {
+
+  static OutlineInputBorder _border(Color color) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 3
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
+
+
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Palette.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border(Palette.borderColor),
+      focusedBorder: _border(Palette.gradient2),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: Palette.backgroundColor,
+    ),
+  );
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stopify/core/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      title: 'Stopify',
+      theme: AppTheme.darkThemeMode,
+      // home: const Dashboard(),
+    );
   }
 }
